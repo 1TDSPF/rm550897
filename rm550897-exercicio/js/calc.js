8babee08cbdfd498ef74d89fe2d06e84c74ffae0
DanielStuart10 <rm550897@fiap.com.br>
2023-05-17 11:06:38 -0300

    commit da calculadora

--- rm550897-exercicio/js/calc.js
+++ rm550897-exercicio/js/calc.js
@@ -1,24 +1,21 @@
 
 function insereNumero(botao,visor) {
     
-    if(botao.value == "+" || botao.value == "-" || botao.value == "/"|| botao.value == "*"){
-        if(visor.value[visor.value.length - 1] == "+" || visor.value[visor.value.length - 1] == "-" ||visor.value[visor.value.length - 1] == "/" ||visor.value[visor.value.length - 1] == "*"){
-            visor.value[visor.value.length - 1] += botao.value;
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+        
+
+        if(visor.value[visor.value.length-1] == "+" ||  visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/"){
+            visor.value.slice(visor.value.length-1);
         }else{
             visor.value += botao.value;
         }
+
     }else{
         visor.value += botao.value;
     }
+
 }
 
 function resultado(visor) {
-    if(visor.value[visor.value.length - 1] == "+" || visor.value[visor.value.length - 1] == "-" ||visor.value[visor.value.length - 1] == "/" ||visor.value[visor.value.length - 1] == "*"){
-        visor.value[visor.value.length - 1] += "";
-        visor.value = eval(visor.value);
-    }else{
-
-        visor.value = eval(visor.value);
-    }
+    visor.value = eval(visor.value);
 }
-
